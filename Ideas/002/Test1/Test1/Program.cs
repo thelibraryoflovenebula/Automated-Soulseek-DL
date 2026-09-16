4b96ec31b011d7d7d2ddccf79ad09f31fa05f37a
love, nebula <nejinekun@gmail.com>
2026-09-16 14:42:41 -0400

    practicing
    text file manipulation

---
+++ Ideas/002/Test1/Test1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test1
+{
+    internal class Program
+    {
+        //Main Method
+        static void Main(string[] args)
+        {
+
+            bool running = true;
+
+            while (running)
+            {
+                Console.Clear();
+
+                Console.Write("Enter a path to read txt contents [x to quit]:>>> ");
+                string path = RemoveQuot(Console.ReadLine());
+                Console.WriteLine("\n" + path);
+
+                
+                if (path.ToUpper() == "X")
+                {
+                    running = false;
+                    Console.WriteLine("\nBye...\n");
+                } else
+                {
+                    //if the path exists in my pc
+                    if (File.Exists(path))
+                    {
+                        //store contents into it
+                        string content = File.ReadAllText(path);
+
+                        //output contents into console
+                        Console.WriteLine("\nFile Found\nContent>>>> ");
+                        Console.WriteLine("\n" + content);
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nFile not found");
+                    }
+                }
+                
+                Console.Write("\nPress any key to continue");
+                Console.ReadKey(); //wait for any key to be pressed
+
+            }
+            
+
+        }
+
+
+
+        //Method to remove front and end quotations
+        static string RemoveQuot(string s)
+        {
+            int sSize = s.Length;
+
+            // double or single quoates 
+            char sQ = '\'';
+            char dQ = '"';
+
+            //if there is two ""... 
+            if (s[0] == sQ && s[sSize - 1] == sQ ||
+                s[0] == dQ && s[sSize - 1] == dQ)
+            {
+                s = s.Remove((sSize - 1), 1);
+                s = s.Remove(0, 1);
+            }
+
+            return s; // return string
+
+        }
+
+    }
+}
---
+++ Ideas/003/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string textFile = @"C:\Users\patri\Desktop\GITHUB PROJECTS\Automated Soulseek DL\Ideas\003\ConsoleApp1\ConsoleApp1\fileChange.txt";
+
+            bool running = true;
+            if (File.Exists(textFile))
+            {
+                while(running)
+                {
+                    Console.Clear();        //Clears the console!
+
+                    string contents = File.ReadAllText(textFile);
+
+                    Console.WriteLine(">>> The current contents of the file are");
+                    Console.WriteLine("\t" + contents);
+                    Console.Write(">>> Change text contents? [y/n] ");
+
+                    string option = Console.ReadLine();
+
+                    switch(option.ToUpper())
+                    {
+                    
+                        case "Y":       //user says yes
+                            Console.WriteLine(">>> Entered desired changed text");
+                            Console.Write("\t");
+                            string newContent = Console.ReadLine();
+                            File.WriteAllText(textFile, newContent);
+
+                            Console.WriteLine(">>> Text file changed !!!");
+
+                            break;
+                        case "N":       //user says no
+                            running = false;
+                            break;
+                        default:
+                            Console.Error.WriteLine(">>> Invalid option entered");
+                            break;
+                    }
+                    Console.Write("\n>>> Hit a key to continue ... ");
+                    Console.ReadKey();   // Will wait for any key to be pressed 
+                }
+            } else
+            {
+                Console.WriteLine(">>> Something went wrong...");
+            }
+
+            Console.Write(">>> Have a great day...");
+
+        }
+    }
+}
